90d6b8f766899f097f645c014b8177f2df8a6aad
Milad <javad.alghosi7904@gmail.com>
2022-06-10 12:15:17 -0700

    test

--- next.config.js
+++ next.config.js
@@ -1,70 +1,73 @@
 const {
   PHASE_DEVELOPMENT_SERVER,
   PHASE_PRODUCTION_BUILD,
-} = require('next/constants')
+} = require("next/constants");
 
 module.exports = (phase) => {
-
   // npm run dev or next dev
   const isDev = phase === PHASE_DEVELOPMENT_SERVER;
 
   // npm run build or next build
-  const isProd = phase === PHASE_PRODUCTION_BUILD && process.env.STAGING !== '1';
+  const isProd =
+    phase === PHASE_PRODUCTION_BUILD && process.env.STAGING !== "1";
 
   // npm run build or next build
-  const isStaging = phase === PHASE_PRODUCTION_BUILD && process.env.STAGING === '1';
+  const isStaging =
+    phase === PHASE_PRODUCTION_BUILD && process.env.STAGING === "1";
 
   const env = {
-      TITLE: (() => {
-          if(isDev) return 'Title Dev'
-          if(isProd) return 'Title Prod'
-          if(isStaging) return 'Title Stg'
-      })()
-  }
+    TITLE: (() => {
+      if (isDev) return "Title Dev";
+      if (isProd) return "Title Prod";
+      if (isStaging) return "Title Stg";
+    })(),
+  };
 
-  const basePath = '/app'
+  const basePath = "/app";
 
   const rewrites = () => {
-      return [
-          {
-              source: '/ab',
-              destination: '/about'
-          }
-      ]
-  }
+    return [
+      {
+        source: "/ab",
+        destination: "/about",
+      },
+    ];
+  };
 
   const redirects = () => {
-      return [
-          {
-              source: '/home',
-              destination: '/',
-              permanent: true
-          }
-      ]
-  }
+    return [
+      {
+        source: "/home",
+        destination: "/",
+        permanent: true,
+      },
+    ];
+  };
 
   const headers = () => {
-      return [
+    return [
+      {
+        source: "/about",
+        headers: [
           {
-              source: '/about',
-              headers: [
-                  {
-                      key: "x-custom-header-1",
-                      value: "my custom header 1"
-                  }
-              ]
-          }
-      ]
-  }
+            key: "x-custom-header-1",
+            value: "my custom header 1",
+          },
+        ],
+      },
+    ];
+  };
 
-  const assetPrefix = isProd? 'https://cdn.mydomain.com': ''
+  const assetPrefix = isProd
+    ? process.env.domain ?? "https://correct.com"
+    : "https://isnotwindow.com";
 
   return {
-      env,
-      basePath,
-      rewrites,
-      redirects,
-      headers,
-      assetPrefix
-  }
-}
+    env,
+    basePath,
+    rewrites,
+    redirects,
+    headers,
+    assetPrefix,
+  };
+};
